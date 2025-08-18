ef5dd392d1dbd1793d432c53c9931eb93f71f093
faisal-shohag <faisalshohagprog@gmail.com>
2025-08-18 20:31:36 +0600

    updated

--- scripts/utils/deep-equal.js
+++ scripts/utils/deep-equal.js
@@ -1,6 +1,12 @@
 // Helper function for deep comparison of objects and arrays
 const isDeepEqual = (a, b) => {
     if (a === b) return true;
+    if (
+        (typeof a === "number" && typeof b === "string" && a == b) ||
+        (typeof a === "string" && typeof b === "number" && a == b)
+    ) {
+        return true;
+    }
     if (a && b && typeof a === 'object' && typeof b === 'object') {
         if (Array.isArray(a) !== Array.isArray(b)) return false;
         const keysA = Object.keys(a);
